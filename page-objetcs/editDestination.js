daf64345ec72f6b5a0ddc009b5517ee517c23309
Cleiber Melo <Cleiber.Melo@primaverabss.com>
2021-02-24 11:45:21 +0000

    enviando codigo do projeto para branch ci

--- page-objetcs/editDestination.js
+++ page-objetcs/editDestination.js
@@ -4,14 +4,14 @@ const randomNumberBetweenOneAnd = require('../utils/randomNumberBetweenOneAndN')
 const { browser } = require('protractor')
 
 class EditDestination {
-    constructor() {
-        this.self = new DestinationComponent()
-        this.form = new FormComponent()
-    }
+  constructor () {
+    this.self = new DestinationComponent()
+    this.form = new FormComponent()
+  }
 
-    visit () {
-        browser.get(`/destinations/${randomNumberBetweenOneAnd(15)}/edit`)
-    }
+  visit () {
+    browser.get(`/destinations/${randomNumberBetweenOneAnd(15)}/edit`)
+  }
 }
 
 module.exports = EditDestination
--- specs/destination.spec.js
+++ specs/destination.spec.js
@@ -3,16 +3,16 @@ const helper = require('protractor-helper')
 const Destination = require('../page-objetcs/destination')
 
 describe('Given Im at a random destination page', () => {
-    let destination
+  let destination
 
-    beforeEach(() => {
-        destination = new Destination()
-        destination.visit()
-    });
-    it('Then I see an image, a heading, a paragraph, and an anchor', () => {
-        helper.waitForElementVisibility(destination.self.image)
-        helper.waitForElementVisibility(destination.self.heading)
-        helper.waitForElementVisibility(destination.self.paragraph)
-        helper.waitForElementVisibility(destination.self.anchor)
-    });
-});
+  beforeEach(() => {
+    destination = new Destination()
+    destination.visit()
+  })
+  it('Then I see an image, a heading, a paragraph, and an anchor', () => {
+    helper.waitForElementVisibility(destination.self.image)
+    helper.waitForElementVisibility(destination.self.heading)
+    helper.waitForElementVisibility(destination.self.paragraph)
+    helper.waitForElementVisibility(destination.self.anchor)
+  })
+})
